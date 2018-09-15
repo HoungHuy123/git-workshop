6acb562450692ccab716ea6499e205e21720dd99
HoungHuy Hourt <hounghuy123@csu.fullerton.edu>
2018-09-14 17:59:41 -0700

    add hello world program

---
+++ gitworkshop/gitworkshop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitworkshop
+//
+//  Created by Huy Houng on 9/14/18.
+//  Copyright © 2018 hounghuy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
